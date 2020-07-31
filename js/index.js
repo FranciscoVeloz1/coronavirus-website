7fd9f2c587634a017cfdd99476a15e86404fd29c
Francisco <franciscoveloz245@gmail.com>
2020-07-30 21:07:36 -0500

    Ahora los datos estaran actualizados automaticamente

--- js/index.js
+++ js/index.js
@@ -3,12 +3,10 @@ let info_muertes = document.getElementById("info-muertes")
 let info_casos = document.getElementById("info-casos")
 let info_recuperados = document.getElementById("info-recuperados")
 
-//Prueba con la api de star wars
+//Prueba con la api de covid19
 const API = "https://api.covid19api.com/country/mexico";
 
 const date = new Date()
-
-console.log(date.getDate())
 
 fetch(`${API}`)
   .then((response) => {
@@ -16,10 +14,13 @@ fetch(`${API}`)
   })
 
   .then((data) => {
-    console.log(data)
-    info_muertes.innerHTML = `-${data[189].Deaths}`
-    info_casos.innerHTML = `-${data[189].Confirmed}`
-    info_recuperados.innerHTML = `-${data[189].Recovered}`
+
+    const tamaño = data.length - 1
+
+    info_muertes.innerHTML = `${data[tamaño].Deaths}`
+    info_casos.innerHTML = `${data[tamaño].Confirmed}`
+    info_recuperados.innerHTML = `${data[tamaño].Recovered}`
+
   })
 
   .catch((error) => console.log(error));
